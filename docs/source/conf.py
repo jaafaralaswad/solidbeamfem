e3ddd3233a294feaaaa6ec346185a65399648a43
Jaafar Alaswad <jalaswad@bu.edu>
2025-12-30 18:41:03 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,4 +6,10 @@ extensions = [
 ]
 
 html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    "repository_url": "https://github.com/jaafaralaswad/solidbeamfem",
+    "use_repository_button": True,
+}
+
 exclude_patterns = []
